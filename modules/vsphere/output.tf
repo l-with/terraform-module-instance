6e0da09ff5902fc24078934fd6fd3671586091db
Lars With <lars@with.de>
2023-09-26 17:18:21 +0200

    output ip_address depending on ipv4_address_var

--- modules/vsphere/output.tf
+++ modules/vsphere/output.tf
@@ -5,12 +5,16 @@ output "instance" {
 
 output "instance_ip_address" {
   description = "the ip address of the instance"
-  value       = !var.instance ? null : vsphere_virtual_machine.instance[0].default_ip_address
+  value = (!var.instance ? null :
+    (var.ipv4_address_var ? var.ipv4_address : vsphere_virtual_machine.instance[0].default_ip_address)
+  )
 }
 
 output "instance_decoupled_ip_address" {
   description = "the decoupled ip address of the instance (default is instance_ip_address)"
-  value       = !var.instance ? null : vsphere_virtual_machine.instance[0].default_ip_address
+  value = (!var.instance ? null :
+    (var.ipv4_address_var ? var.ipv4_address : vsphere_virtual_machine.instance[0].default_ip_address)
+  )
 }
 
 output "vsphere_folder" {
