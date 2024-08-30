222e4dfbe7726ab3c9d9723468ca104acc8f4ba6
Lars With <lars@with.de>
2024-08-30 09:45:26 +0300

    terraform fmt

--- modules/scaleway/versions.tf
+++ modules/scaleway/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3"
   required_providers {
     scaleway = {
-      source = "scaleway/scaleway"
+      source  = "scaleway/scaleway"
       version = "~> 2.43"
     }
   }
